f11a1f0949fca2e0093ecc4b8e9829762d1d9120
mrcsporto <mrcsporto@gmail.com>
2022-06-02 20:51:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+	.then(response => {
+		return response.json()
+})
+	.then(async data => {
+        const page = document.querySelector("#pokedex-page")
+		const box = document.querySelector("#pokemon-box")
+		page.innerHTML = ""
+	
+		for(let i = 0; i < data.results.length; i++){
+			box.querySelector("#pokemon-name").innerHTML = data.results[i].name
+			box.querySelector("#pokemon-name").style.textTransform = "capitalize"
+
+			const pokemonImage = await fetch("https://pokeapi.co/api/v2/pokemon-form/" + data.results[i].name)
+			const image = await pokemonImage.json()
+			box.querySelector("#pokemon-img").src = image.sprites.front_default
+			page.innerHTML += box.outerHTML		
+	}
+})
